d77a01b49513fba1341c8c6dd20e25757d426dfc
Lorenz <lorenz.kassewalder95@gmail.com>
2021-02-19 09:34:17 +0100

    Commit for graml

---
+++ src/net/htlgrieskirchen/aud4/election/Klasse.java
@@ -0,0 +1,13 @@
+package net.htlgrieskirchen.aud4.election;
+
+public class Klasse {
+    String methodName;
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public void setMethodName(String methodName) {
+        this.methodName = methodName;
+    }
+}
--- src/net/htlgrieskirchen/aud4/election/Parser.java
+++ src/net/htlgrieskirchen/aud4/election/Parser.java
@@ -9,26 +9,62 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Parser {
+    Map<Integer, Integer> edgeMap = new HashMap<>();
+    List<String> commandList = new ArrayList<>();
+    Klasse klasse = new Klasse();
     public void read(String filename) throws IOException, ParserConfigurationException, SAXException {
         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
         Document doc = dBuilder.parse(new File(filename));
+        doc.getDocumentElement().normalize();
         NodeList nodes = doc.getElementsByTagName("node");
         for (int i = 0; i < nodes.getLength(); i++) {
-            //System.out.println(((Element) nodes.item(i)).getElementsByTagName("y:NodeLabel").item(0).getTextContent() + i);
+            String id = ((Element) nodes.item(i)).getAttribute("id");
+            Element element = ((Element) nodes.item(i));
+            String nameSpace = element.getElementsByTagName("y:NodeLabel").item(0).getTextContent().trim();
+            if(i == 0){
+                klasse.methodName = nameSpace;
+            }
 
+            System.out.println(nameSpace);
+            commandList.add(nameSpace);
         }
+
         NodeList edges = doc.getElementsByTagName("edge");
+        int targetInt = 0;
+        int sourceInt = 0;
         for (int i = 0; i < nodes.getLength(); i++) {
             //System.out.println(((Element) nodes.item(i)).getElementsByTagName("y:NodeLabel").item(0).getTextContent());
             String source = (((Element) edges.item(i)).getAttribute("source"));
             String target = (((Element) edges.item(i)).getAttribute("target"));
             String[] sourceSplit = source.split("::");
-            source = sourceSplit[1].replace("n", "");
+            String[] targetSplit = target.split("::");
+            sourceInt = Integer.parseInt(sourceSplit[1].replace("n", ""));
 
-
+            if (targetSplit.length == 2) {
+                targetInt = Integer.parseInt(targetSplit[1].replace("n", ""));
+            }
+            else{
+                targetInt = -1;
+            }
+            System.out.println("S: "+source);
+            System.out.println("T: "+target);
+            edgeMap.put(sourceInt, targetInt);
         }
+        System.out.println("");
+    }
+    public void paste(){
+        for (int i = 0; i < commandList.size(); i++) {
+            int target = edgeMap.get(i);
+            System.out.println(commandList.get(i));
+            //System.out.println(nodeList.get(i+1).getMessage());
+        }
+
     }
 }
